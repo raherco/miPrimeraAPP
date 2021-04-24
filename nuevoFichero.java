73eabfb2c8d0b098ac1eb7ae472b27c58dc5367f
raherco <raquel_rhc@hotmail.com>
2021-04-24 20:40:40 +0200

    Subida desde nuevaRama

--- nuevoFichero.java
+++ nuevoFichero.java
@@ -3,7 +3,7 @@ public class nuevoFichero {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Modificado desde local PC");
+		System.out.println("Cambiado desde rama nueva");
 	}
 
 }
