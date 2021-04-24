22c2fcb703d8ef6f3026d54536abe46eff769a05
raherco <82973089+raherco@users.noreply.github.com>
2021-04-24 19:55:59 +0200

    modificado desde repositorio

--- nuevoFichero.java
+++ nuevoFichero.java
@@ -3,7 +3,7 @@ public class nuevoFichero {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Fichero modificado 2");
+		System.out.println("Modificado desde repositorio");
 	}
 
 }
